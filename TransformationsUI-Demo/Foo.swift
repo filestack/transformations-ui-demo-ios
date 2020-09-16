b02c322bbcc471731f8794e8967da58f95822e37
Ruben Nine <ruben@9labs.io>
2020-09-16 16:27:40 +0200

    Updating demo projects.

---
+++ TransformationsUI-Demo/Foo.swift
@@ -0,0 +1,8 @@
+//
+//  Foo.swift
+//  TransformationsUI-Demo
+//
+//  Created by Ruben Nine on 09/09/2020.
+//
+
+import Foundation
--- TransformationsUI-Demo/ViewController.swift
+++ TransformationsUI-Demo/ViewController.swift
@@ -7,12 +7,11 @@
 
 import UIKit
 import TransformationsUI
-import TransformationsUIPremiumAddOns
 
 class ViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var editedImageView: UIImageView!
-
+    
     // MARK: - Lifecycle Functions
 
     override func viewDidLoad() {
@@ -26,24 +25,9 @@ class ViewController: UIViewController {
     @IBAction func presentTransformationsUI(_ sender: AnyObject) {
         guard let image = imageView.image else { return }
 
-        let modules: PremiumModules
-
-        do {
-            modules = try PremiumModules(apiKey: "YOUR-API-KEY")
-        } catch {
-            debugPrint("Error: \(error)")
-            return
-        }
-
-        modules.transform.cropCommands.append(
-            PremiumModules.Transform.Commands.Crop(type: .rect, aspectRatio: .original)
-        )
-
-        modules.transform.cropCommands.append(
-            PremiumModules.Transform.Commands.Crop(type: .rect, aspectRatio: .custom(CGSize(width: 16, height: 9)))
-        )
-
-        let transformationsUI = TransformationsUI(with: Config(modules: modules))
+        let modules = StandardModules()
+        let config = Config(modules: modules)
+        let transformationsUI = TransformationsUI(with: config)
 
         transformationsUI.delegate = self
 

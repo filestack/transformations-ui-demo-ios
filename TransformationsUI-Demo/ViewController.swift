6ce0f69ca5c9bd3c01e2b00d3fbd93a968e48501
Ruben Nine <ruben@9labs.io>
2020-12-18 13:35:54 +0100

    Updating demo to build against TransformationsUI 1.1.2.

--- TransformationsUI-Demo/ViewController.swift
+++ TransformationsUI-Demo/ViewController.swift
@@ -1,17 +1,18 @@
 //
 //  ViewController.swift
-//  TransformationsUI-Demo
+//  TransformationsUI-Premium-Demo
 //
 //  Created by Ruben Nine on 29/06/2020.
 //
 
 import UIKit
 import TransformationsUI
+import TransformationsUIPremiumAddOns
 
 class ViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var editedImageView: UIImageView!
-    
+
     // MARK: - Lifecycle Functions
 
     override func viewDidLoad() {
@@ -25,8 +26,34 @@ class ViewController: UIViewController {
     @IBAction func presentTransformationsUI(_ sender: AnyObject) {
         guard let image = imageView.image else { return }
 
-        let modules = StandardModules()
-        let config = Config(modules: modules)
+        let config: Config
+
+        do {
+            let premiumModules = try PremiumModules(apiKey: "YOUR-API-KEY-HERE")
+
+            premiumModules.transform.cropCommands.append(
+                PremiumModules.Transform.Commands.Crop(type: .rect, aspectRatio: .original)
+            )
+
+            premiumModules.transform.cropCommands.append(
+                PremiumModules.Transform.Commands.Crop(type: .rect, aspectRatio: .custom(CGSize(width: 16, height: 9)))
+            )
+
+            premiumModules.text.availableFontFamilies.append(contentsOf: ["Optima Regular", "Symbol", "Tahoma"])
+
+            premiumModules.sticker.stickers = [
+                "Elegant 1": (01...18).compactMap { UIImage(named: "stickers-elegant-\($0)") },
+                "Elegant 2": (19...36).compactMap { UIImage(named: "stickers-elegant-\($0)") },
+                "Elegant 3": (37...54).compactMap { UIImage(named: "stickers-elegant-\($0)") },
+                "Elegant 4": (55...72).compactMap { UIImage(named: "stickers-elegant-\($0)") },
+                "Elegant 5": (71...90).compactMap { UIImage(named: "stickers-elegant-\($0)") },
+            ]
+
+            config = Config(modules: premiumModules)
+        } catch {
+            config = Config()
+        }
+
         let transformationsUI = TransformationsUI(with: config)
 
         transformationsUI.delegate = self
